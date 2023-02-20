9298f91483bfa08a0bd46ad712fb9653b57316a0
juliflorezg <forezjuli08@hotmail.com>
2023-02-20 15:57:21 -0500

    add routes & stack navigator

---
+++ src/pages/PermissionsScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export const PermissionsScreen = () => {
+  return (
+    <View>
+      <Text>PermissionsScreen</Text>
+    </View>
+  );
+};
