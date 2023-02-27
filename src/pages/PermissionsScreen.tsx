46c3866a5004d03f1eb0580e58758116893e49f4
Julian Florez <forezjuli08@hotmail.com>
2023-02-27 11:40:30 -0500

    add react-native-permissions dependency & edit android manifest to include location permissions

--- src/pages/PermissionsScreen.tsx
+++ src/pages/PermissionsScreen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 export const PermissionsScreen = () => {
+  // Los permisos no son 100% necesarios ya que si el usuario no concede permiso para el GPS, podemos obtener la localización usando la IP, pero esto crea un margen de error muy grande
+  // https://www.npmjs.com/package/react-native-permissions
+
   return (
     <View>
       <Text>PermissionsScreen</Text>
